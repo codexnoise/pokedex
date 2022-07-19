eca4adf3439e50caf23acccac816b25ab81cb569
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-07-19 00:07:21 -0500

    add: library Ant Design and Searcher component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { Col } from "antd";
+import Searcher from "./components/Searcher";
 
 function App() {
-  return <div className="App">POKEDUX PROJECT</div>;
+  return (
+    <div className="App">
+      POKEDUX PROJECT
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+    </div>
+  );
 }
 
 export default App;
