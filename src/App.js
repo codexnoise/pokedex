306028de5290d71b3ecf92a9e22b6f19d6ebbeba
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-07-19 22:57:17 -0500

    add: api for get the info of the pokemons

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import "./App.css";
 import { Col } from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
+import logo from "./static/logo.svg";
+import { useEffect, useState } from "react";
+import { getPokemons } from "./api";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fecthPokemons = async () => {
+      const pokemonsRes = await getPokemons();
+      setPokemons(pokemonsRes);
+    };
+    fecthPokemons();
+  }, []);
+
   return (
     <div className="App">
-      POKEDUX PROJECT
+      <Col span={5} offset={10}>
+        <img src={logo} alt="pokedux" />
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList />
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,14 +2,14 @@ import { StarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import Meta from "antd/lib/card/Meta";
 
-const PokemonCard = () => {
+const PokemonCard = ({ name }) => {
   return (
     <Card
-      title="Pikachu"
+      title={name}
       cover={
         <img
           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png"
-          alt="Picachu"
+          alt={name}
         />
       }
       extra={<StarOutlined />}
