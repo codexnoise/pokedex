860b544d8f228ddee009a7e547160210da8300b2
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2023-05-31 21:53:30 -0500

    add: BrowserRouter, Routes and Route components

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import {
 import { logger } from "./middleware";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
+import { BrowserRouter, Route, Routes  } from "react-router-dom";
 
 const composeAlernative =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -20,10 +21,17 @@ const composedEnhancers = composeAlernative(applyMiddleware(thunk, logger));
 const store = createStore(rootReducer, composedEnhancers);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <h1>Hello Word</h1>
     <Provider store={store}>
-      <App />
+    <BrowserRouter >
+        <Routes>
+          <Route path="/pokedex" component={App} />
+          <Route path="/" component={App} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
+    <h1>God Bye Word</h1>
   </React.StrictMode>
 );
